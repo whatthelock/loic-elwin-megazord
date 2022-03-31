9e07a25ba6be79dfbb4decf8c732d2304f1481cd
Loïc Delorme <locdelorme@gmail.com>
2022-03-30 23:57:46 -0400

    gsap addtition in index

--- scripts/home.js
+++ scripts/home.js
@@ -1,21 +1,37 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
+  direction: 'horizontal',
+  pagination: {
       el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+      type: 'bullets',
+      bulletActiveClass: 'activeBullet',
+      clickable: 'true'
+  },
+
+})
+
+gsap.registerPlugin(ScrollTrigger);
+
+let sectionArr = document.querySelectorAll('section');
+sectionArr.forEach(function(section) {
+  console.log(section)
+  let titreArr = section.querySelectorAll('h2');
+  let elementArr = section.querySelectorAll('.row:nth-child(2)');
+    titreArr.forEach(function(header) {
+      elementArr.forEach(function(element) {
+        console.log(titreArr)
+        gsap.timeline({
+          scrollTrigger: {
+            trigger: section,
+            toggleActions: 'restart complete reverse reset',
+          },
+        })
+        .fromTo(header, { y: '100%', opacity: '0%' },
+        { y: '0%', opacity: '100%' , duration: 0.5})
+
+        .fromTo(element, { y: '100%', opacity: '0%' },
+        { y: '0%', opacity: '100%'}, '-=0.3')
+      });
+    });
+});
+
+    
