04c1c48dc58aa20e3926c79a7bc38a3a56c6c61f
Elwin-Durand <elwindurand@outlook.com>
2022-03-30 23:19:06 -0400

    animation section scrolltrigger
    animation section scrolltrigger avec du scaling i guess

---
+++ scripts/discography.js
@@ -0,0 +1,47 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+let section = document.querySelectorAll('section')
+
+section.forEach(function(section){
+
+    
+
+  gsap.timeline({
+    scrollTrigger: {
+      start: 'top 90%',
+      end: 'bottom 90%',
+      trigger: section,
+      toggleActions: 'restart complete reverse reset',
+    }
+  })
+
+.from(section, {opacity: 0})
+.to(section, {opacity: 100})
+
+gsap.timeline({
+    scrollTrigger: {
+      trigger: 'video',
+      toggleActions: 'restart complete reverse reset',
+    }
+  })
+  
+    .from('video', {scale: 0})
+    .to('video', {scale: 1})
+    
+    
+  let images = section.querySelectorAll('section img');
+  images.forEach(function(img){
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: img,
+        toggleActions: 'restart complete reverse reset',
+      }
+    })
+
+      .from(img, { scale: 0 })
+      .to(img, { scale: 1 })
+  });
+
+  
+});
